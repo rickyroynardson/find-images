a531fda3226fd1e3079e3a70766d7033ad57e963
Ricky Roy <rickyroynardson@gmail.com>
2022-05-24 23:41:15 +0700

    setup & install tailwindcss

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="bg-white p-3 flex items-center justify-between">
+            <p>Left</p>
+            <p>Right</p>
+        </header>
+    );
+};
+
+export default Header;
