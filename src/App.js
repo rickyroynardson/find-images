bc8f2f0e3faa8705240bb447cebb573fab4a2d81
Ricky Roy <rickyroynardson@gmail.com>
2022-05-26 16:38:57 +0700

    create load more function

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import Image from "./components/Image";
 
 function App() {
     const [images, setImages] = useState([]);
+    const [count, setCount] = useState(16);
 
     useEffect(() => {
-        fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&per_page=30`)
+        fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&per_page=${count}`)
             .then((res) => res.json())
             .then((data) => {
                 setImages(data.hits);
             })
             .catch((err) => console.log(err));
-    }, []);
+    }, [count]);
+
+    const loadMore = () => {
+        setCount(count + 8);
+    }
 
     return (
         <div className="bg-gray-200 min-h-screen p-5">
@@ -23,8 +28,12 @@ function App() {
                     <Image key={image.id} image={image} />
                 ))}
             </div>
+            <div className="text-center my-5 space-y-1">
+                <p className="text-gray-700 font-semibold">Showing <span className="text-blue-700">{count}</span> of <span className="text-blue-700">200</span></p>
+                <button onClick={loadMore} className="bg-blue-600 text-white text-sm font-semibold py-1 px-3 rounded-full hover:scale-105">Load more...</button>
+            </div>
             <Footer />
-        </div>
+        </div >
     );
 }
 
